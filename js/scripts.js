3928bb9382bc9cb5ab0b9ada8f4636789a9debbf
shy <shyamal.punekar@gmail.com>
2017-07-07 15:33:53 -0700

    test logic to all branches

--- js/scripts.js
+++ js/scripts.js
@@ -31,38 +31,43 @@ $(document).ready(function () {
 
   $("#button").on("click", function (event) {
     var count = 0;
+    var address = $(".address").append("400 SW 6th Ave #800, Portland, OR 97204");
     var fullNameInput = $("#fullName").val();
     var answerOne = $("#questionOne").val();
     if (answerOne == 'h6') {
       count = count + 1;
     }
     var answerTwo = $("#questionTwo").val();
-    if (answerTwo == 'h6') {
+    if (answerTwo == '<br/>') {
       count = count + 1;
     }
 
     var answerThree = $("#questionThree").val();
-    if (answerThree == 'h6') {
+    if (answerThree == 'client scripting') {
       count = count + 1;
     }
     var answerFour = $("#questionFour").val();
-    if (answerFour == 'h6') {
+    if (answerFour == 'style') {
       count = count + 1;
     }
     var answerFive = $("#questionFive").val();
-    if (answerFive == 'h6') {
+    if (answerFive == 'true') {
       count = count + 1;
     }
     if (count >= 3) {
-      alert('hi');
+      alert('Form Submitted');
+    } else {
+      alert("Please, enter other details");
     }
     $(".fullName").text(fullNameInput);
     $(".questionOne").text(answerOne);
     $(".CSSDesign").text(answerTwo);
 
 
-    $("#suggestedCourse").show();
+    $("#suggestedCourse").toggle();
+    $("#meetUs").toggle();
     event.preventDefault();
+
   });
 
 });
